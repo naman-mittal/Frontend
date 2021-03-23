e9fab1d2372adc31b97a8bbb1831e96a2f504f66
Naman-Mtal <namanmittal153@gmail.com>
2021-03-23 14:21:07 +0530

    added drawer

--- sample_proj/src/App.js
+++ sample_proj/src/App.js
@@ -1,23 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import VerticalTabs from './components/VerticalTabs';
+import ButtonAppBar from './components/ButtonAppBar';
+import Drawer from './components/MainDrawer'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <Drawer/>
     </div>
   );
 }
